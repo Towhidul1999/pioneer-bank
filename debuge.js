8159d8f86bbd35bec52e565d06d59cc90ae404ea
Towhidul1999 <76882148+Towhidul1999@users.noreply.github.com>
2021-01-27 18:39:26 +0600

    Delete debuge.js

--- debuge.js
+++
@@ -1,4 +0,0 @@
-const withDrawBtn = document.getElementById("addWithDraw");
-withDrawBtn.addEventListener("click", function(){
-    const withDrawAmount = document.getElementById("withDrawAmount").value;
-    const withDrawNumber = parseFloat(withDrawAmount);
